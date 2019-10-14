402625b0314b4463707d1d7dd9a354d534522290
Eduardo Neto <edu.neto@icloud.com>
2019-10-14 13:01:23 +0100

    added function defenition to generate random password and declared an object with different character sets

---
+++ logic.js
@@ -0,0 +1,18 @@
+//FUNCTION DEFINITION TO CREATE RANDOM PASSWORD
+function randomPassword(string, n) {
+	let str = '';
+	while (str.length < n) {
+		str += string[Math.floor(Math.random() * string.length)];
+	}
+	return str;
+}
+
+//OBJECT DEFINITION WITH DIFFERENT CHARACTER SETS
+const charset = {
+	fullSet:
+		'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=',
+	lowerCase: 'abcdefghijklmnopqrstuvwxyz',
+	upperCase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+	nums: '0123456789',
+	specialCharacters: '!@#$%^&*()_+~`|}{[]:;?><,./-=',
+};
